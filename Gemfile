2ec3e80854bf30564fa99c0861a4a40ef549987b
Tim Petricola <tim.petricola@gmail.com>
2016-04-04 22:44:35 +0200

    Init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'rspotify'
+gem 'sinatra'
+gem 'omniauth'
+gem 'redis'
---
+++ app.rb
@@ -0,0 +1,43 @@
+require 'rspotify'
+require 'rspotify/oauth'
+require 'sinatra'
+require 'omniauth'
+require 'redis'
+
+REDIS = Redis.new
+RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+
+SPOTIFY_ID_TRACK_REGEX = /(?:open\.spotify\.com\/track\/|spotify:track:)(\w+)/i
+
+use Rack::Session::Cookie
+use OmniAuth::Builder do
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'playlist-read-private playlist-modify-public playlist-modify-private'
+end
+
+get '/' do
+  redirect to('/auth/spotify')
+end
+
+get '/auth/spotify/callback' do
+  REDIS.set('oauth', request.env['omniauth.auth'].to_json)
+  request.env['omniauth.auth'].to_json
+end
+
+post '/slack-incoming' do
+  return if params[:token] != ENV['SLACK_TOKEN']
+
+  track_id = SPOTIFY_ID_TRACK_REGEX.match(params[:text])[1]
+
+  return unless track_id
+
+  credentials = JSON.parse(REDIS.get('oauth'))
+  user = RSpotify::User.new(credentials)
+
+  playlist = RSpotify::Playlist.find(user.id, ENV['SPOTIFY_PLAYLIST_ID'])
+  track =  RSpotify::Base.find(track_id, 'track')
+
+  puts
+
+  playlist.add_tracks!([track])
+  nil
+end
